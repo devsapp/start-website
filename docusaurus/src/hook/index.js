e2b568c475c2d80f25d90d27092223d335f59431
heimanba <yuankun.yk@alibaba-inc.com>
2022-04-14 19:12:47 +0800

    update reademe

--- docusaurus/src/hook/index.js
+++ docusaurus/src/hook/index.js
@@ -1,24 +1,41 @@
+const path = require('path');
+
 async function preInit(inputObj) {
     console.log(`\n  ____                                                  
- |  _ \\  ___   ___ _   _ ___  __ _ _   _ _ __ _   _ ___ 
- | | | |/ _ \\ / __| | | / __|/ _\` | | | | '__| | | / __|
- | |_| | (_) | (__| |_| \\__ \\ (_| | |_| | |  | |_| \\__ \\
- |____/ \\___/ \\___|\\__,_|___/\\__,_|\\__,_|_|   \\__,_|___/
+    |  _ \\  ___   ___ _   _ ___  __ _ _   _ _ __ _   _ ___ 
+    | | | |/ _ \\ / __| | | / __|/ _\` | | | | '__| | | / __|
+    | |_| | (_) | (__| |_| \\__ \\ (_| | |_| | |  | |_| \\__ \\
+    |____/ \\___/ \\___|\\__,_|___/\\__,_|\\__,_|_|   \\__,_|___/
+        
+  Serverless Devs Application Case
+    
+    Cloud services required：
+    - FC : https://fc.console.aliyun.com/
+    
+    Tips：
+    - FC Component: https://www.serverless-devs.com/fc/readme`)
 
-     
-                                      `)
+    try {
+        var process = require('child_process')
+        const version = (await process.execSync('s -v')).toString()
+        const versionNumber = version.match(/s: 2\.0\.(.*?),/)[1]
+        if (Number(versionNumber) < 103) {
+            console.log('\x1B[31m%s\x1B[0m', '    * The application requires that the version of Serverless Devs is at least 2.0.103')
+            console.log('\x1B[31m%s\x1B[0m', '    * Plaese upgraded through [npm install -g @serverless-devs/s]\n\n')
+        }
+    } catch (e) {
+        console.log(e)
+        console.log(`    - Serverless Devs Version >= v2.0.103
+        `)
+    }
 }
 
 async function postInit(inputObj) {
-
-    console.log(`\n    Welcome to the website static application
-     This application requires to open these services: 
-         OSS : https://oss.console.aliyun.com/
-         CDN : https://cdn.console.aliyun.com/
-
-     * 部署前执行：npm install --production
-       如果遇到npm命令找不到等问题，可以适当进行手动项目构建，并根据需要取消actions内容 
-     * 项目初始化完成，您可以直接进入项目目录下，并使用 s deploy 进行项目部署\n`)
+    console.log(`
+    * Before using, please check whether the actions command in Yaml file is available
+    * Carefully reading the notes in s.yaml is helpful for the use of the tool
+    * If need help in the use process, please apply to join the Dingtalk Group: 33947367
+    `)
 }
 
 module.exports = {
